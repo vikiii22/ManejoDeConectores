89e33343a4461afdfa9dca817596ab95d085bb06
JoseASL <joseviki10@gmail.com>
2021-11-12 20:37:25 +0100

    Practica antes de examen

--- src/main/java/com/connectores/mysql/LoginBaseDatos.java
+++ src/main/java/com/connectores/mysql/LoginBaseDatos.java
@@ -38,41 +38,57 @@ public class LoginBaseDatos {
         return logins;
     }
 
-    public void verTodo() throws SQLException {
+    public void verTodo() {
         String sql = "SELECT * FROM manejoconectores.users";
-        PreparedStatement statement = con.prepareStatement(sql);
-        for (Login login : getLogins()) {
-            System.out.println("-----------------------------------------------");
-            System.out.println("|" + login.getIdUser() + " " + login.getName() + " " + login.getPais() + " " + login.getEdad() + " " + login.getPassword() + " " + login.getModelo());
-            System.out.println("-----------------------------------------------");
+        try {
+            PreparedStatement statement = con.prepareStatement(sql);
+            for (Login login : getLogins()) {
+                System.out.println("-----------------------------------------------");
+                System.out.println("|" + login.getIdUser() + " " + login.getName() + " " + login.getPais() + " " + login.getEdad() + " " + login.getPassword() + " " + login.getModelo());
+                System.out.println("-----------------------------------------------");
+            }
+            statement.executeQuery(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        statement.executeQuery(sql);
     }
 
-    public void crearUsuario(String user, String password, String pais, int edad) throws SQLException {
+    public void crearUsuario(String user, String password, String pais, int edad) {
         String sql = "INSERT INTO manejoconectores.users(name, password, pais, edad)\n" +
                 "\tVALUES(\"" + user + "\", \"" + password + "\", \"" + pais + "\", " + edad + ");";
-        PreparedStatement statement = con.prepareStatement(sql);
-        statement.executeUpdate(sql);
+        try {
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    public Boolean existeUsuario(String user, String password) throws SQLException {
+    public Boolean existeUsuario(String user, String password) {
         boolean existe = false;
-        for (Login login : getLogins()) {
-            if (login.getName().equals(user) && login.getPassword().equals(password)) {
-                existe = true;
+        try {
+            for (Login login : getLogins()) {
+                if (login.getName().equals(user) && login.getPassword().equals(password)) {
+                    existe = true;
+                }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return existe;
     }
 
-    public void eliminarUsuario(int id) throws SQLException {
+    public void eliminarUsuario(int id) {
         String sql = "DELETE FROM manejoconectores.users WHERE idUser = " + id + ";";
-        for (Login login : getLogins()) {
-            if (login.getIdUser() == id) {
-                PreparedStatement statement = con.prepareStatement(sql);
-                statement.executeUpdate(sql);
+        try {
+            for (Login login : getLogins()) {
+                if (login.getIdUser() == id) {
+                    PreparedStatement statement = con.prepareStatement(sql);
+                    statement.executeUpdate(sql);
+                }
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         System.out.println("Eliminado con éxito");
     }
@@ -85,22 +101,26 @@ public class LoginBaseDatos {
         System.out.println("Usuario modificado con éxito");
     }
 
-    public void edadMedia() throws SQLException {
+    public void edadMedia() {
         String sql = "SELECT avg(edad) FROM manejoconectores.users;";
-        Statement statement=con.createStatement();
-        ResultSet rs=statement.executeQuery(sql);
-        if (rs.next()){
-            System.out.println("La media de edad de los compradores es: " + rs.getInt(1) + " años");
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        try {
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            if (rs.next()) {
+                System.out.println("La media de edad de los compradores es: " + rs.getInt(1) + " años");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
-    public void usuariosRegistrados(){
-        String sql="SELECT COUNT(idUser) FROM manejoconectores.users;";
+    public void usuariosRegistrados() {
+        String sql = "SELECT COUNT(idUser) FROM manejoconectores.users;";
         try {
             Statement statement = con.createStatement();
             ResultSet rs = statement.executeQuery(sql);
@@ -112,54 +132,65 @@ public class LoginBaseDatos {
                     e.printStackTrace();
                 }
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public void miZonaDeEnvio(String name) throws SQLException {
-        String sql="SELECT pais FROM manejoconectores.users WHERE name=\""+name+"\";";
-        Statement statement= con.createStatement();
-        ResultSet rs=statement.executeQuery(sql);
-        if (rs.next()){
-            System.out.println("Tu zona de envío es: "+rs.getString(1));
+    public void miZonaDeEnvio(String name) {
+        String sql = "SELECT pais FROM manejoconectores.users WHERE name=\"" + name + "\";";
+        try {
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            if (rs.next()) {
+                System.out.println("Tu zona de envío es: " + rs.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
-    public void eliminarTodo(){
+    public void eliminarTodo() {
         System.out.println("Seguro?");
     }
 
-    public void modificarVehiculoSeleccionado(int id, String modelo) throws SQLException {
-        String sql="UPDATE manejoconectores.users SET cocheSeleccionado=\""+modelo+"\"\n" +
-                "\tWHERE idUser="+id+";";
-        System.out.println(sql);
-        PreparedStatement statement= con.prepareStatement(sql);
-        statement.executeUpdate(sql);
-        System.out.println("Vehículo modificado con éxtio");
-    }
-
-    public void localizarMiID(String nombre) throws SQLException {
-        String sql="SELECT idUser FROM manejoconectores.users WHERE name=\""+nombre+"\"";
-        Statement statement= con.createStatement();
-        ResultSet rs=statement.executeQuery(sql);
-        if (rs.next()){
-            System.out.println("Tu ID es: "+rs.getString(1));
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+    public void modificarVehiculoSeleccionado(int id, String modelo) {
+        String sql = "UPDATE manejoconectores.users SET cocheSeleccionado=\"" + modelo + "\"\n" +
+                "\tWHERE idUser=" + id + ";";
+        try {
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.executeUpdate(sql);
+            System.out.println("Vehículo modificado con éxtio");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
-    public void busquedaPorID(int id){
-        String sql="SELECT * FROM manejoconectores.users WHERE idUser="+id+";";
+    public void localizarMiID(String nombre) {
+        String sql = "SELECT idUser FROM manejoconectores.users WHERE name=\"" + nombre + "\"";
         try {
-            Statement statement= con.createStatement();
-            ResultSet rs=statement.executeQuery(sql);
-            for (Login login:getLogins()){
-                if (rs.next()){
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            if (rs.next()) {
+                System.out.println("Tu ID es: " + rs.getString(1));
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void busquedaPorID(int id) {
+        String sql = "SELECT * FROM manejoconectores.users WHERE idUser=" + id + ";";
+        try {
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            for (Login login : getLogins()) {
+                if (rs.next()) {
                     login.getAll();
                     Thread.sleep(2000);
                 }
